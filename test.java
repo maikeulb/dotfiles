b494eb284f27475a8ef00dc54cab10a6819898ce
maikeulb <michael.sergio.barnes@gmail.com>
2022-01-25 11:07:57 -0500

    cleanup

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+
+  static int solve(int[] a) {
+    return 1;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {1, 1, 0, 0};
+    System.out.println(solve(nums)); // 4
+  }
+}
+
