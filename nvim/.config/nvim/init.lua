291a372902e7b5652bc993bcbd6e5e1c848493a9
maikeulb <michael.sergio.barnes@gmail.com>
2022-02-23 23:43:53 -0500

    refactor and snippets

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,8 +4,10 @@ require("functions")
 require("lsp_config")
 require("lualine_config")
 require("settings")
-require("plugins_settings")
 require("mappings")
+require("plugins_settings")
 require("autocommands")
 require("lightspeed_config")
 vim.cmd 'source ~/.config/nvim/vimscript/functions.vim'
+
+
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -38,9 +38,8 @@ return packer.startup(function(use)
   use 'nvim-lua/plenary.nvim'
 
   -- Treesitter
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+  use {'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
   }
 
   -- LSP
@@ -50,6 +49,7 @@ return packer.startup(function(use)
   use 'tami5/lspsaga.nvim'
   use 'mfussenegger/nvim-jdtls'
   use 'simrat39/rust-tools.nvim'
+  use 'jackguo380/vim-lsp-cxx-highlight'
 
   -- Completions
   use 'hrsh7th/nvim-cmp'
@@ -57,6 +57,8 @@ return packer.startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
+
+  -- Snippets
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'
 
@@ -70,10 +72,11 @@ return packer.startup(function(use)
   -- Windows
   use "vim-scripts/winpos.vim"
   use 'blueyed/vim-diminactive'
-  use {"beauwilliams/focus.nvim", config = function() require("focus").setup() end }
+  use {'beauwilliams/focus.nvim',
+    config = function() require("focus").setup() end
+  }
   use 'voldikss/vim-floaterm'
-  use {
-    'romgrk/barbar.nvim',
+  use {'romgrk/barbar.nvim',
     requires = {'kyazdani42/nvim-web-devicons'}
   }
   use "numToStr/FTerm.nvim"
@@ -112,9 +115,8 @@ return packer.startup(function(use)
 
   -- Navigation
   use 'tpope/vim-abolish'
-  use {'junegunn/fzf', run = function()
-      vim.fn['fzf#install']()
-      end
+  use {'junegunn/fzf',
+    run = function() vim.fn['fzf#install']() end
   }
   use 'junegunn/fzf.vim'
   use 'nanotee/zoxide.vim'
@@ -128,13 +130,8 @@ return packer.startup(function(use)
 
   -- Folds
   use 'Konfekt/FastFold'
-  use{
-    "gbprod/cutlass.nvim",
-    config = function()
-      require("cutlass").setup({
-          cut_key = "m"
-      })
-    end
+  use{'gbprod/cutlass.nvim',
+    config = function() require("cutlass").setup({ cut_key = "m" }) end
   }
 
   -- Formatting
@@ -148,8 +145,7 @@ return packer.startup(function(use)
   use {'derekwyatt/vim-fswitch', ft = {'c', 'cpp'}}
 
   -- Remote editing
-  use {
-    'chipsenkbeil/distant.nvim',
+  use {'chipsenkbeil/distant.nvim',
     config = function()
       require('distant').setup {
         -- Applies Chip's personal settings to every machine you connect to
